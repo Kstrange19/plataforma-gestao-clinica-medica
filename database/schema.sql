af005bb7c8b8448a0ed0a312b749fd690fd8e815
Kauã Amado <kauaamado5@gmail.com>
2025-11-25 08:54:09 -0300

    refactor: atualiza constraints

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 -- =============================================
 -- Projeto: Plataforma de Gestão para Clínica Médica
 -- Descrição: Criação de banco de dados para gerenciar pacientes, consultas e disponibilidade.
--- Autores: Kauã Amado (Kstrange19) e Moisés Henrique (moises.h7)
+-- Autores: Kauã Amado (Kstrange19) e Moisés Henrique (moises-h7)
 -- Data: 02/12/2025
 -- =============================================
 CREATE DATABASE IF NOT EXISTS clinica_medica;
@@ -10,16 +10,16 @@ USE clinica_medica;
 CREATE TABLE IF NOT EXISTS medicos (
         id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         nome VARCHAR(255) NOT NULL,
-        especialidade VARCHAR(50),
-        telefone VARCHAR(11),
-        email VARCHAR(255)
+        especialidade VARCHAR(50) NOT NULL,
+        telefone VARCHAR(11) UNIQUE NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL
     );
 
 CREATE TABLE IF NOT EXISTS clientes (
         id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         nome VARCHAR(255) NOT NULL,
         idade INT NOT NULL,
-        email VARCHAR(100),
+        email VARCHAR(100) UNIQUE NOT NULL,
         telefone VARCHAR(11)
     );
 
@@ -43,4 +43,3 @@ CREATE TABLE IF NOT EXISTS disponibilidade_medicos (
     FOREIGN KEY (medico_id) REFERENCES medicos(id),
     UNIQUE KEY unique_disponibilidade (medico_id, dia_semana, horario_inicio)
 );
-
